c2f20fbc1bc13438294148fe42b6282f140b18d1
Ryan Arana <ryan@ryanarana.com>
2014-10-15 01:50:39 -0700

    get some tilt action in there.

--- game/stick.lua
+++ game/stick.lua
@@ -10,6 +10,7 @@ function Stick:new( x, y )
     s.controller = controller
     s.grabbed = false
     s.touchOffset = { 0, 0 }
+    s.dir = 1
 
     return s
 end
@@ -19,10 +20,10 @@ function Stick.loadMesh()
     local tint = { 255, 205, 135, }
     local size = { love.graphics.getWidth() * 0.046875, love.graphics.getHeight() * 0.66667 }
     local coords = {
-        left = 0,
-        right = size[1],
-        top = 0,
-        bottom = size[2],
+        left = -size[1]/2,
+        right = size[1]/2,
+        top = -size[2]/2,
+        bottom = size[2]/2,
     }
     local uv = {
         left = 0.0,
@@ -60,15 +61,28 @@ end
 
 function Stick:draw()
     love.graphics.push()
-    love.graphics.translate( -self.w/2, -self.h/2 )
+    if self.grabbed then
+        local offsetX = self.x + self.touchOffset[1]
+        local offsetY = self.y + self.touchOffset[2]
+        love.graphics.translate( offsetX, offsetY )
+        love.graphics.rotate( self.dir * math.pi/16 )
+        love.graphics.translate( -offsetX, -offsetY )
+        love.graphics.translate( -self.dir * (self.touchOffset[2]/(self.h/2)) * 100, 0 )
+    end
     love.graphics.draw( self.mesh, self.x, self.y )
     love.graphics.pop()
 end
 
 function Stick:update()
     if self.grabbed then
+        local prevX = self.x
         self.x = love.mouse.getX() + self.touchOffset[1]
-        self.y = love.mouse.getY() - self.touchOffset[2]
+        self.y = love.mouse.getY() + self.touchOffset[2]
+        if self.x - prevX < -2 then
+            self.dir = -1
+        elseif self.x - prevX > 2 then
+            self.dir = 1
+        end
     end
 end
 
@@ -80,7 +94,8 @@ function Stick:mousepressed( x, y, button, isTouch )
     self.grabbed = self:isInside( x, y )
     if self.grabbed then
         self.touchOffset[1] = self.x - x
-        self.touchOffset[2] = y - self.y
+        self.touchOffset[2] = self.y - y
+        self.dir = 0
     end
 end
 
